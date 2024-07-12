ce28b35d1b0a2f5c254eda96e814809fff265c3b
Haiyang <haiyang@ampere.lix.polytechnique.fr>
2024-07-12 20:11:11 +0200

    exclude external lib

--- models/pointbert/misc.py
+++ models/pointbert/misc.py
@@ -7,17 +7,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 import os
 from collections import abc
-from pointnet2_ops import pointnet2_utils
+# from pointnet2_ops import pointnet2_utils
 
-
-# def fps(data, number):
-#     '''
-#         data B N 3
-#         number int
-#     '''
-#     fps_idx = pointnet2_utils.furthest_point_sample(data, number)
-#     fps_data = pointnet2_utils.gather_operation(data.transpose(1, 2).contiguous(), fps_idx).transpose(1,2).contiguous()
-#     return fps_data
 
 def index_points(points, idx):
     """
@@ -37,36 +28,36 @@ def index_points(points, idx):
     new_points = points[batch_indices, idx, :]
     return new_points
 
-# def fps(xyz, npoint):
-#     """
-#     Input:
-#         xyz: pointcloud data, [B, N, 3]
-#         npoint: number of samples
-#     Return:
-#         centroids: sampled pointcloud index, [B, npoint]
-#     """
-#     device = xyz.device
-#     B, N, C = xyz.shape
-#     centroids = torch.zeros(B, npoint, dtype=torch.long).to(device)
-#     distance = torch.ones(B, N).to(device) * 1e10
-#     farthest = torch.randint(0, N, (B,), dtype=torch.long).to(device)
-#     batch_indices = torch.arange(B, dtype=torch.long).to(device)
-#     for i in range(npoint):
-#         centroids[:, i] = farthest
-#         centroid = xyz[batch_indices, farthest, :].view(B, 1, 3)
-#         dist = torch.sum((xyz - centroid) ** 2, -1)
-#         distance = torch.min(distance, dist)
-#         farthest = torch.max(distance, -1)[1]
-#     return index_points(xyz, centroids)
+def fps(xyz, npoint):
+    """
+    Input:
+        xyz: pointcloud data, [B, N, 3]
+        npoint: number of samples
+    Return:
+        centroids: sampled pointcloud index, [B, npoint, 3]
+    """
+    device = xyz.device
+    B, N, C = xyz.shape
+    centroids = torch.zeros(B, npoint, dtype=torch.long).to(device)
+    distance = torch.ones(B, N).to(device) * 1e10
+    farthest = torch.randint(0, N, (B,), dtype=torch.long).to(device)
+    batch_indices = torch.arange(B, dtype=torch.long).to(device)
+    for i in range(npoint):
+        centroids[:, i] = farthest
+        centroid = xyz[batch_indices, farthest, :].view(B, 1, 3)
+        dist = torch.sum((xyz - centroid) ** 2, -1)
+        distance = torch.min(distance, dist)
+        farthest = torch.max(distance, -1)[1]
+    return index_points(xyz, centroids)
 
-def fps(data, number):
-    '''
-        data B N 3
-        number int
-    '''
-    fps_idx = pointnet2_utils.furthest_point_sample(data, number) 
-    fps_data = pointnet2_utils.gather_operation(data.transpose(1, 2).contiguous(), fps_idx).transpose(1,2).contiguous()
-    return fps_data
+# def fps(data, number):
+#     '''
+#         data B N 3
+#         number int
+#     '''
+#     fps_idx = pointnet2_utils.furthest_point_sample(data, number) 
+#     fps_data = pointnet2_utils.gather_operation(data.transpose(1, 2).contiguous(), fps_idx).transpose(1,2).contiguous()
+#     return fps_data
 
 def worker_init_fn(worker_id):
     np.random.seed(np.random.get_state()[1][0] + worker_id)
